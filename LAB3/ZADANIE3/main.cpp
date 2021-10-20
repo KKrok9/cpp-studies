99c7a7df87f27ff5619a9e716353a9f324bba3bb
KKrok9 <kacperodespero.01@wp.pl>
2021-10-20 15:46:08 +0200

    2 commit

--- LAB3/ZADANIE3/main.cpp
+++ LAB3/ZADANIE3/main.cpp
@@ -1,17 +1,41 @@
 #include <iostream>
-
+#include <cstdlib>
+#include <time.h>
 using namespace std;
-int w;
+int rozmiar;
+int wartosc_maksymalna ;
+void losowanie(int rozmiar, int *tablica, int wartosc_maksymalna)
+{
+
+    cout << "Wylosowane liczby to:" <<endl ;
+    for(int i =0; i<rozmiar; i++)
+    {
+        tablica[i] = (rand()%100) + 1;
+        if(tablica[i]>wartosc_maksymalna)
+        {
+            wartosc_maksymalna=tablica[i];
+            }
+        cout << tablica[i] << " ";
+
+    }
+   cout<<"\nNajwieksza wartosc w powyzszej tablicy to :"<< wartosc_maksymalna<<endl;
+}
+
+
 int main()
 {
-    int w,k ; // wiersze i kolumny
-    int i ; // pom
-    cout<<"Liczba wierszy: ";
-    cin>>w;
-    cout<<"Liczba kolumn: ";
-    cin>>k;
+srand(time(NULL));
+
+ cout << "Podaj rozmiar tablicy :";
+ cin >> rozmiar;
+
+ int* tablica = new int [rozmiar];
+
+    losowanie(rozmiar,tablica,wartosc_maksymalna);
 
 
 
+
+delete[] tablica;
     return 0;
 }
