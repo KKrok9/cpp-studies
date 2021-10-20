754ce7b5269e2a8f0a8f8247dfff115bf996aa29
KKrok9 <kacperodespero.01@wp.pl>
2021-10-20 18:59:32 +0200

    2 commit

---
+++ LAB3/ZADANIE7/main.cpp
@@ -0,0 +1,56 @@
+//Do napisania jest funkcja do sortowania liczb, które są zapisane w zmiennych w rosnącej kolejności. Należy użyć wskaźników
+#include <iostream>
+using namespace std;
+
+int a = 70 ;
+int b = 100;
+int c = 50;
+void insert_sort(int* tab)
+{
+
+    for (int i = 1; i < 3; i++)
+    {
+       int x = tab[i];
+       int j = i - 1;
+        while (j >= 0 && tab[j] > x)
+        {
+            tab[j + 1] = tab[j];
+            --j;
+        }
+        tab[j + 1] = x;
+
+
+    }
+}
+int main()
+{
+    int *wsk_a = &a;
+    int *wsk_b = &b;
+    int *wsk_c = &c;
+
+    int* tab = new int [3]; // alokacja
+
+    tab[0] = *wsk_a; // przypisanie wartosci pod wskaznikiem do elementow tablicy
+    tab[1] = *wsk_b;
+    tab[2] = *wsk_c;
+    cout << "Przed sortowaniem : "<< endl;
+
+for(int i = 0; i <=2 ; i++)
+{
+    cout << tab[i] <<endl; // wypisywanie
+}
+
+    cout << "Po sortowaniu : "<< endl ;
+
+    insert_sort(tab); // insertion
+
+    for(int i = 0; i <=2 ; i++)
+    {
+        cout << tab[i] <<endl;// wypisywanie 2
+    }
+
+    delete [] tab;
+return 0;
+}
+
+
--- cmake-build-debug/LAB3/CMakeFiles/main.dir/DependInfo.cmake
+++ cmake-build-debug/LAB3/CMakeFiles/main.dir/DependInfo.cmake
@@ -8,7 +8,7 @@ set(CMAKE_DEPENDS_LANGUAGES
   )
 # The set of files for implicit dependencies of each language:
 set(CMAKE_DEPENDS_CHECK_CXX
-  "C:/Users/Kacper/CLionProjects/JiPP2/LAB3/ZADANIE6/main.cpp" "C:/Users/Kacper/CLionProjects/JiPP2/cmake-build-debug/LAB3/CMakeFiles/main.dir/ZADANIE6/main.cpp.obj"
+  "C:/Users/Kacper/CLionProjects/JiPP2/LAB3/ZADANIE7/main.cpp" "C:/Users/Kacper/CLionProjects/JiPP2/cmake-build-debug/LAB3/CMakeFiles/main.dir/ZADANIE7/main.cpp.obj"
   )
 set(CMAKE_CXX_COMPILER_ID "MSVC")
 
