faaeaca2bba2c4c2ee5a412797e2e3b9a1efc4df
KKrok9 <kacperodespero.01@wp.pl>
2021-10-20 19:16:58 +0200

    2 commit

---
+++ LAB3/ZADANIE9/main.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+using namespace std ;
+int a, b;
+void dzielenie(int a , int b)
+{
+    if(a<b)
+    {
+        cout << "Liczba a musi byc wieksza niz b "<< endl;
+    }
+
+    else if(a%b != 0)
+    {
+        cout << "Liczba a nie jest podzielna przez b";
+    }
+
+    else
+    {
+        cout << "\nWynik dzielenia to :" << a/b<< endl;
+    }
+}
+int main()
+{
+cout << "               DZIELENIE LICZB NATURALNYCH "<< endl ;
+
+cout << "Podaj liczbe a :"<< endl;
+cin >> a;
+
+cout << "Podaj liczbe b :"<< endl;
+cin >> b;
+
+dzielenie(a,b);
+
+
+}
+
+
+
