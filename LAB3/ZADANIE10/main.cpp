7a079375d0fc0da8d104f9ffbca95ebf78c722ec
KKrok9 <kacperodespero.01@wp.pl>
2021-10-23 14:30:58 +0200

    2 commit

--- LAB3/ZADANIE10/main.cpp
+++ LAB3/ZADANIE10/main.cpp
@@ -6,8 +6,15 @@ int main() {
     char* tab = new char[32];
     std :: cout << "Podaj tekst do konwersji :";
     std :: cin.getline(tab, 32);
+    throw "Nie mozna przekonwertowac na liczbe";
+    try
+    {
+        int atoi(const char *tab);
+        std::cout << "Tekst po konwersji na int wyglada nastepujaco : " << atoi;
+    }
+   catch()
+   {
 
-    int atoi( const char * tab );
-    std :: cout << "Tekst po konwersji na int wyglada nastepujaco : "<< atoi ;
+   }
     return 0;
 }
