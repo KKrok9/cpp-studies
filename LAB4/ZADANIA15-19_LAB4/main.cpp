f4159e7e2326d67e739d5791379840637706e90f
KKrok9 <kacperodespero.01@wp.pl>
2021-11-04 17:27:49 +0100

    2 commit

--- LAB4/ZADANIA15-19_LAB4/main.cpp
+++ LAB4/ZADANIA15-19_LAB4/main.cpp
@@ -1,12 +1,25 @@
 #include <iostream>
 #include <string>
 using namespace std;
+// Nie idzie mi cos z tymi destruktorami :(((
+class Zadania
+{
+public:
+    Zadania();
+    ~Zadania();
+};
 
 int main()
 {
+    Zadania *Zadaniaa;
+    Zadaniaa = new Zadania;
 
-
+    delete Zadaniaa;
     return 0;
+}
+Zadania :: Zadania()
+{
+    cout<< "Konstruktor Wywolany."<<endl;
 }
 
 
