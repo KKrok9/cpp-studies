9ac3ffb0c9bfdfb55c5adeda12f1a80e30aa6c1b
KKrok9 <kacperodespero.01@wp.pl>
2021-11-16 10:02:41 +0100

    2 commit

--- LAB6/FRIENDLY_FUNCTIONS/src/main.cpp
+++ LAB6/FRIENDLY_FUNCTIONS/src/main.cpp
@@ -1,9 +1,12 @@
 #include <iostream>
+#include "../include/Node.h"
 using namespace std;
 
 int main()
 {
-
+    Node a(3,6);
+    Node b (2,10);
+    cout << "Odleglosc miedzy punktami wynosi "<< pointsDistance(a,b);
 
     return 0;
 }
